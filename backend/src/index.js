cf11dec4c4d902c245e09473d2251e02e197e405
hochan222 <201502766@hufs.ac.kr>
2019-09-07 16:37:19 +0900

    add morgan(logger) & nodemon.json to exe js graphql

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
-import mongoose from 'mongoose';
 import { GraphQLServer } from 'graphql-yoga'
+import mongoose from 'mongoose';
+import logger from 'morgan'
 require('dotenv').config()
 
 import schema from './schema'
@@ -10,7 +11,7 @@ const PORT = process.env.PORT || 3000
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost"
 
 const server = new GraphQLServer({ schema })
-
+server.express.use(logger("dev"))
 server.start((PORT) => console.log(`Server is running`))
 
 // mongodb
